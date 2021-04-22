ff1f35963550885488bf5cf07572d3f6888ba953
Kevin <5137496-Carke@users.noreply.gitlab.com>
2021-04-22 15:21:08 +0200

    First commit

---
+++ src/sample/HelloWorld.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class HelloWorld {
+    private String message = "Hello World!";
+
+    HelloWorld(){}
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
